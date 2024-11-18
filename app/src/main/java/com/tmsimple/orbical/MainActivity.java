438de9c9ae4f46f23bddc67bc40fbf3a1259d18f
Alex Louis <alouis2202@gmail.com>
2024-11-17 22:42:35 -0500

    first commit

--- app/src/main/java/com/tmsimple/orbical/MainActivity.java
+++ app/src/main/java/com/tmsimple/orbical/MainActivity.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.text.DateFormat;
@@ -10,9 +11,11 @@ import java.util.Calendar;
 //comment added
 public class MainActivity extends AppCompatActivity {
     private Button open;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -21,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
 
         TextView dateTextView = findViewById(R.id.date_text);
         dateTextView.setText(dateFormat);
+
+        imageView = findViewById(R.id.monday);
+        imageView = findViewById(R.id.tuesday);
+        imageView = findViewById(R.id.wednesday);
+        imageView = findViewById(R.id.thursday);
+        imageView = findViewById(R.id.friday);
+        imageView = findViewById(R.id.saturday);
+        imageView = findViewById(R.id.sunday);
+        Calendar cal = Calendar.getInstance();
+
+        if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+            imageView.setImageResource(R.drawable.monday);
+        }else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.TUESDAY) {
+            imageView.setImageResource(R.drawable.tuesday);
+        }else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.WEDNESDAY) {
+            imageView.setImageResource(R.drawable.wednesday);
+        }else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.THURSDAY) {
+            imageView.setImageResource(R.drawable.thursday);
+        }else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
+            imageView.setImageResource(R.drawable.friday);
+        }else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+            imageView.setImageResource(R.drawable.saturday);
+        }else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+            imageView.setImageResource(R.drawable.sunday);
+        }
 
         open = findViewById(R.id.button1);
         open.setOnClickListener(new View.OnClickListener() {
