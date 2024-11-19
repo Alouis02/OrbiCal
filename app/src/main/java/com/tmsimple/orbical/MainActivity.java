74346417f985078a71f8f486d3f0cdb5ddf7dde0
moxie <rwatrous_2668@email.ric.edu>
2024-11-18 20:07:37 -0500

    Day of the year marker rotation

--- app/src/main/java/com/tmsimple/orbical/MainActivity.java
+++ app/src/main/java/com/tmsimple/orbical/MainActivity.java
@@ -2,18 +2,16 @@ package com.tmsimple.orbical;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.text.DateFormat;
+import java.time.Year;
 import java.util.Calendar;
-// additional imports for popup window
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 
 //comment added
 public class MainActivity extends AppCompatActivity {
@@ -39,10 +37,6 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.friday);
         imageView = findViewById(R.id.saturday);
         imageView = findViewById(R.id.sunday);
-
-        imageView = findViewById(R.id.year);
-        imageView = findViewById(R.id.leapyear);
-
         Calendar cal = Calendar.getInstance();
 
         if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
@@ -61,48 +55,34 @@ public class MainActivity extends AppCompatActivity {
             imageView.setImageResource(R.drawable.sunday);
         }
 
-        if (cal.get(Calendar.YEAR) == Calendar.YEAR) {
-            imageView.setImageResource(R.drawable.year);
+    //Code for the day marker rotation
+        RotateAnimation anim = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        Calendar days = Calendar.getInstance();
+        int dayOfYear = days.get(Calendar.DAY_OF_YEAR);
+        int yearNum = Calendar.YEAR;
+        boolean isLeapYear = Year.now().isLeap();
+
+        //January 1st 12:00 AM
+        Calendar newYear = Calendar.getInstance();
+        newYear.set(Calendar.MONTH, 0);
+        newYear.set(Calendar.DATE, 1);
+        newYear.set(Calendar.HOUR_OF_DAY, 0);
+        newYear.set(Calendar.MINUTE,0);
+        newYear.set(Calendar.SECOND,0);
+
+        if (Calendar.getInstance().compareTo(newYear) == 0) { //If it's the new year
+            imageView = findViewById(R.id.daymarker);
+            imageView.animate().rotation(0).rotation(0).start();
+        }else if(isLeapYear) {
+            imageView = findViewById(R.id.daymarker);
+            imageView.animate().rotation(0).rotation(0.9836065573770F*dayOfYear).start();
         }
-        else if (cal.get(Calendar.YEAR) == Calendar.YEAR) {
-            imageView.setImageResource(R.drawable.leapyear);
+        else{
+            imageView = findViewById(R.id.daymarker);
+            imageView.animate().rotation(0).rotation(0.9863013698630F*dayOfYear).start(); //rotation degree == 360/365 multplied by the day of the year when not a leap year
         }
 
-        //open = findViewById(R.id.button1);
-        //open.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View v) {
-              //  Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                //startActivity(intent);
-            //}
-        //});
-    }
-    public void onButtonShowPopupWindowClick(View view) {
-
-        // inflate the layout of the popup window
-        LayoutInflater inflater = (LayoutInflater)
-                getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popup_window, null);
-
-        // create the popup window
-        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true; // lets taps outside the popup also dismiss it
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-
-        // show the popup window
-        // which view you pass in doesn't matter, it is only used for the window tolken
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-        // dismiss the popup window when touched
-        popupView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return true;
-            }
-        });
-
+    //button
         open = findViewById(R.id.button1);
         open.setOnClickListener(new View.OnClickListener() {
             @Override
